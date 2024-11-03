b47f60016ce2f0b4e6d9bd93bf187d644240ef66
unknown <pablo.masquimillan.penailillo@cftmail.cl>
2024-11-03 14:06:21 -0300

    angular_producto-template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PortafolioComponent } from './paginas/portafolio/portafolio.component';
+import { AboutComponent } from './paginas/about/about.component';
+import { ItemComponent } from './paginas/item/item.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "Inicio", component: PortafolioComponent},
+  { path: "about", component: AboutComponent},
+  { path: "item", component: ItemComponent},
+  { path: "**", pathMatch: "full", redirectTo: "Inicio"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { PortafolioComponent } from './paginas/portafolio/portafolio.component';
+import { AboutComponent } from './paginas/about/about.component';
+import { ItemComponent } from './paginas/item/item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PortafolioComponent,
+    AboutComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule,
